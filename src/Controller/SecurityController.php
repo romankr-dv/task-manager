289278fe0bea2dd058383b89a507c36c15c0f81a
yamadote <yamadote@gmail.com>
2023-05-01 03:04:46 +0300

    Fix logging out

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,12 +3,13 @@
 namespace App\Controller;
 
 use App\Form\LoginFormType;
+use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class LoginController extends AbstractController
+class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
@@ -23,5 +24,11 @@ class LoginController extends AbstractController
             'loginForm' => $loginForm->createView(),
             'error' => $authenticationUtils->getLastAuthenticationError()
         ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
