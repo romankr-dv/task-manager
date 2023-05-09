fbd195a60e27723d688cf29c981418a0ff159352
yamadote <yamadote@gmail.com>
2023-05-09 22:21:33 +0300

    Add sidebar search timeout (History page)

--- assets/components/App/Config.js
+++ assets/components/App/Config.js
@@ -2,6 +2,7 @@ const Config = new function () {
   return {
     apiUrlPrefix: "/internal-api",
     updateInputTimeout: 1200,
+    updateSearchTimeout: 300,
     historyActionSpacerTime: 300,
     githubUrlPrefix: "https://github.com/",
     reminderTaskColor: 'rgb(255, 99, 71)',
--- assets/components/App/Sidebar/Search/Search.js
+++ assets/components/App/Sidebar/Search/Search.js
@@ -1,12 +1,14 @@
 import React, {useState} from 'react';
 import "./Search.scss";
+import Helper from "../../Helper";
+import Config from "../../Config";
 
 const Search = ({onSearch}) => {
   const [value, setValue] = useState("");
   const onChange = (e) => {
     const value = e.target.value;
     setValue(value);
-    onSearch(value)
+    Helper.addTimeout("search", () => onSearch(value), Config.updateSearchTimeout)
   }
   let className = "form-control search-query";
   if (value) {
