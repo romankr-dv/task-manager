fcf7dd9d22f3961c9c9a822fa30082fe0d213b1b
yamadote <yamadote@gmail.com>
2023-05-14 02:39:29 +0300

    Remove auto generated comments in migrations

--- migrations/Version20220517195951.php
+++ migrations/Version20220517195951.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20220517195951 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,7 +16,6 @@ final class Version20220517195951 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE action (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, task_id INT DEFAULT NULL, created_at DATETIME NOT NULL, type VARCHAR(20) NOT NULL, message VARCHAR(255) NOT NULL, INDEX IDX_47CC8C92A76ED395 (user_id), INDEX IDX_47CC8C928DB60186 (task_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE action ADD CONSTRAINT FK_47CC8C92A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE action ADD CONSTRAINT FK_47CC8C928DB60186 FOREIGN KEY (task_id) REFERENCES task (id) ON DELETE CASCADE');
@@ -27,7 +23,6 @@ final class Version20220517195951 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE action');
     }
 }
--- migrations/Version20230513202610.php
+++ migrations/Version20230513202610.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20230513202610 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,7 +16,6 @@ final class Version20230513202610 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user_task_settings DROP FOREIGN KEY FK_4B0BE7D98DB60186');
         $this->addSql('ALTER TABLE user_task_settings DROP FOREIGN KEY FK_4B0BE7D9A76ED395');
         $this->addSql('DROP TABLE user_task_settings');
@@ -29,7 +25,6 @@ final class Version20230513202610 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE user_task_settings (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, task_id INT NOT NULL, is_additional_panel_open TINYINT(1) NOT NULL, is_children_open TINYINT(1) NOT NULL, INDEX IDX_4B0BE7D9A76ED395 (user_id), INDEX IDX_4B0BE7D98DB60186 (task_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE user_task_settings ADD CONSTRAINT FK_4B0BE7D98DB60186 FOREIGN KEY (task_id) REFERENCES task (id)');
         $this->addSql('ALTER TABLE user_task_settings ADD CONSTRAINT FK_4B0BE7D9A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
